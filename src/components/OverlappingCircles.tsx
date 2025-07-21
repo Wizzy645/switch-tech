7374c534195ecd959a16e603eaee3daf1c9e642d
Wizzy645 <olamide.adesina@binghamuni.edu.ng>
2025-07-21 13:40:20 +0100

    Updated services layout, fixed styles, added new images

--- src/components/OverlappingCircles.tsx
+++ src/components/OverlappingCircles.tsx
@@ -2,7 +2,7 @@ import Image from "next/image";
 
 const OverlappingCircles = () => {
   return (
-    <div className="flex justify-center items-center flex-wrap gap-4 sm:gap-8 my-16">
+    <div className="flex flex-col sm:flex-row justify-center items-center gap-4 sm:gap-8 my-16">
       {/* Circle 1 */}
       <div className="relative w-40 h-40 sm:w-48 sm:h-48 rounded-full border-[6px] border-yellow-400 overflow-hidden z-10">
         <Image
@@ -14,7 +14,7 @@ const OverlappingCircles = () => {
       </div>
 
       {/* Circle 2 - overlapping */}
-      <div className="relative w-44 h-44 sm:w-56 sm:h-56 rounded-full border-[6px] border-yellow-400 overflow-hidden -ml-6 sm:-ml-12 z-20">
+      <div className="relative w-44 h-44 sm:w-56 sm:h-56 rounded-full border-[6px] border-yellow-400 overflow-hidden z-20 sm:-ml-12">
         <Image
           src="/skyscraper.jpg"
           alt="Skyscraper"
@@ -24,7 +24,7 @@ const OverlappingCircles = () => {
       </div>
 
       {/* Circle 3 */}
-      <div className="relative w-40 h-40 sm:w-48 sm:h-48 rounded-full border-[6px] border-yellow-400 overflow-hidden -ml-6 sm:-ml-12 z-10">
+      <div className="relative w-40 h-40 sm:w-48 sm:h-48 rounded-full border-[6px] border-yellow-400 overflow-hidden z-10 sm:-ml-12">
         <Image
           src="/modern-building.jpg"
           alt="Modern Building"
